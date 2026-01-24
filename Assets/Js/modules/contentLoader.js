9801661783e155fb7ae4614eda89a792cd19fdb9
SkyPowerGB <pyrobrunogb@gmail.com>
2026-01-24 22:10:10 +0100

    fix

--- Assets/Js/modules/contentLoader.js
+++ Assets/Js/modules/contentLoader.js
@@ -40,14 +40,14 @@ async function getFolderCatIndex(srcFolderRel) {
 //  *ConfigJson- > SitePosIndex:  ["src: 0","pages: 1", "contentSub: 2"....], 
 async function loadNavbar(srcFolderRel, sitePosIndex) {
     let configJson = await loaderHelper.getConfigJsonV2(srcFolderRel);
-    navbarHelper.loadNavbar(configJson,sitePosIndex,srcFolderRel);
+    navbarHelper.loadNavbar(configJson, sitePosIndex, srcFolderRel);
 
 }
 
 
 async function loadPageFilters(srcFolderRel, targetComponentId, filterEventCallback) {
- let configJson=await loaderHelper.getConfigJsonV2(srcFolderRel);
-    filtersHelper.loadPageFilters( configJson, targetComponentId, filterEventCallback);
+    let configJson = await loaderHelper.getConfigJsonV2(srcFolderRel);
+    filtersHelper.loadPageFilters(configJson, targetComponentId, filterEventCallback);
 
 }
 
@@ -57,35 +57,35 @@ async function loadPageFilters(srcFolderRel, targetComponentId, filterEventCallb
 //*------------------------------------------------------------------------------------*/
 
 
-async function loadMetadataInto(srcFoldRel,folderCat,folderNm,targetId){
+async function loadMetadataInto(srcFoldRel, folderCat, folderNm, targetId) {
 
-    let configJson=await loaderHelper.getConfigJsonV2(srcFoldRel);
-   let metadata= await loaderHelper.getContentMetadataV2(srcFoldRel,folderCat,folderNm);
+    let configJson = await loaderHelper.getConfigJsonV2(srcFoldRel);
+    let metadata = await loaderHelper.getContentMetadataV2(srcFoldRel, folderCat, folderNm);
 
-   if(metadata==undefined){
-    return;
-   }
+    if (metadata == undefined) {
+        return;
+    }
 
-                let contentFolderRelPth= await loaderHelper.contentFolderLink(configJson,folderCat,folderNm,srcFoldRel);
-
-
-                let dataTxtKey=configJson.contentSetup.metadataReadSetup.optionalTxtSummaryKey;
-                  let summaryTxt="";
-                  let summaryFileNm=metadata[dataTxtKey]
-             
-              
-                 if(summaryFileNm!=undefined){
-                  
-                 summaryTxt=await loaderHelper.getMetadataTxt(contentFolderRelPth,summaryFileNm);
-                 }
-              
-                let component= compBuilder.createSummaryItem(metadata,configJson,contentFolderRelPth,summaryTxt);
-            
-
-                let targetComponent=document.getElementById(targetId);
+    let contentFolderRelPth = await loaderHelper.contentFolderLink(configJson, folderCat, folderNm, srcFoldRel);
 
 
-                targetComponent.appendChild(component);
+    let dataTxtKey = configJson.contentSetup.metadataReadSetup.optionalTxtSummaryKey;
+    let summaryTxt = "";
+    let summaryFileNm = metadata[dataTxtKey]
+
+
+    if (summaryFileNm != undefined) {
+
+        summaryTxt = await loaderHelper.getMetadataTxt(contentFolderRelPth, summaryFileNm);
+    }
+
+    let component = compBuilder.createSummaryItem(metadata, configJson, contentFolderRelPth, summaryTxt);
+
+
+    let targetComponent = document.getElementById(targetId);
+
+
+    targetComponent.appendChild(component);
 
 
 }
@@ -96,56 +96,56 @@ async function loadMetadataInto(srcFoldRel,folderCat,folderNm,targetId){
 async function loadXlatestSummariesIntoV2(summaryNum, folderCat, targetId, srcFoldRel) {
     let configJson = await loaderHelper.getConfigJsonV2(srcFoldRel);
 
-    let adressMapSetup=configJson.contentSetup.adressMapSetup;
-    let adressMapMaxIndex= adressMapSetup.adressMapsIndexing[folderCat].count;
-    let noDataIndex=adressMapSetup.noDataIndex;
+    let adressMapSetup = configJson.contentSetup.adressMapSetup;
+    let adressMapMaxIndex = adressMapSetup.adressMapsIndexing[folderCat].count;
+    let noDataIndex = adressMapSetup.noDataIndex;
 
-    let loadedSummaries=0;
+    let loadedSummaries = 0;
 
-    for(let ami=noDataIndex+1;ami<=adressMapMaxIndex;ami++){
+    for (let ami = noDataIndex + 1; ami <= adressMapMaxIndex; ami++) {
 
-      let  adressMapLoadResult=await loaderHelper.getContentAdressMap(srcFoldRel,folderCat,ami);
-        if(adressMapLoadResult.status==1){
-       
-            let adressMap= await adressMapLoadResult.result;
-          
+        let adressMapLoadResult = await loaderHelper.getContentAdressMap(srcFoldRel, folderCat, ami);
+        if (adressMapLoadResult.status == 1) {
+
+            let adressMap = await adressMapLoadResult.result;
 
 
-            let adressMapKeys=Object.keys(adressMap);
 
-            
-           
+            let adressMapKeys = Object.keys(adressMap);
 
-            for(let ki=adressMapKeys.length-1;ki>=0;ki--){
-                if(loadedSummaries==summaryNum){
+
+
+
+            for (let ki = 0; ki < adressMapKeys.length; ki++) {
+                if (loadedSummaries == summaryNum) {
                     break;
                 }
-              
 
 
-                let key=adressMapKeys[ki]
-                let adress=adressMap[key];
-        
 
-                let folderName=adress.folderName
+                let key = adressMapKeys[ki]
+                let adress = adressMap[key];
 
-                
-            
-                await loadMetadataInto(srcFoldRel,folderCat,folderName,targetId);
+
+                let folderName = adress.folderName
+
+
+
+                await loadMetadataInto(srcFoldRel, folderCat, folderName, targetId);
                 loadedSummaries++;
 
-                 
+
 
             }
-            
+
         }
     }
 
 
 
-    
 
-    
+
+
 
 
 
@@ -154,55 +154,55 @@ async function loadXlatestSummariesIntoV2(summaryNum, folderCat, targetId, srcFo
 let pageNum = 0;
 let loadedItems = new Set();
 
-function refreshLoaded(){
-    loadedItems=new Set();
+function refreshLoaded() {
+    loadedItems = new Set();
 }
-async function loadPaginatedSummaries(srcFoldRel, pageNum,folderCat, targetId) {
-  
-
-    
-       let configJson = await loaderHelper.getConfigJsonV2(srcFoldRel);
-
-    let adressMapSetup=configJson.contentSetup.adressMapSetup;
-    let adressMapMaxIndex= adressMapSetup.adressMapsIndexing[folderCat].count;
-    let noDataIndex=adressMapSetup.noDataIndex;
-    let summaryNum=configJson.contentSetup.contentDataSetup.itemsPerPage;
-    let skip=pageNum*summaryNum;
-
-    let loadedSummaries=0;
+async function loadPaginatedSummaries(srcFoldRel, pageNum, folderCat, targetId) {
 
 
-    for(let ami=noDataIndex+1;ami<=adressMapMaxIndex;ami++){
 
-    
-      let  adressMapLoadResult=await loaderHelper.getContentAdressMap(srcFoldRel,folderCat,ami);
+    let configJson = await loaderHelper.getConfigJsonV2(srcFoldRel);
 
-        if(adressMapLoadResult.status==1){
-   
-            let adressMap= await adressMapLoadResult.result;
-          
+    let adressMapSetup = configJson.contentSetup.adressMapSetup;
+    let adressMapMaxIndex = adressMapSetup.adressMapsIndexing[folderCat].count;
+    let noDataIndex = adressMapSetup.noDataIndex;
+    let summaryNum = configJson.contentSetup.contentDataSetup.itemsPerPage;
+    let skip = pageNum * summaryNum;
 
-            let adressMapKeys=Object.keys(adressMap);
+    let loadedSummaries = 0;
 
-           
 
-          for (let key of adressMapKeys) {
-    let adress = adressMap[key];
-    if (loadedItems.has(adress.folderName)) continue; // skip already loaded
+    for (let ami = noDataIndex + 1; ami <= adressMapMaxIndex; ami++) {
 
-    if (!filtersHelper.checkFiltersV2(adress.filters)) continue; // skip non-matching
 
-    await loadMetadataInto(srcFoldRel, folderCat, adress.folderName, targetId);
-    loadedItems.add(adress.folderName);
-    loadedSummaries++;
+        let adressMapLoadResult = await loaderHelper.getContentAdressMap(srcFoldRel, folderCat, ami);
 
-    if (loadedSummaries >= summaryNum) break; // page full
-}
-            
+        if (adressMapLoadResult.status == 1) {
+
+            let adressMap = await adressMapLoadResult.result;
+
+
+            let adressMapKeys = Object.keys(adressMap);
+
+
+
+            for (let key of adressMapKeys) {
+                let adress = adressMap[key];
+                if (loadedItems.has(adress.folderName)) continue;
+
+                if (!filtersHelper.checkFiltersV2(adress.filters)) continue;
+
+                await loadMetadataInto(srcFoldRel, folderCat, adress.folderName, targetId);
+                loadedItems.add(adress.folderName);
+                loadedSummaries++;
+
+                if (loadedSummaries >= summaryNum) break;
+            }
+
         }
 
 
-   
+
     }
 
 }
